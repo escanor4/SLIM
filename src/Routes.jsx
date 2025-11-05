3dc6d23b9e41c23962caa6c20cb73a28cdaaee94
escanor4 <berrachedayoub84@gmail.com>
2025-11-05 22:44:28 +0100

    routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -9,29 +9,29 @@ import AnalyseDesPerformances from './pages/analyse-des-performances';
 import CyclesSterilisation from './pages/cycles-de-sterilisation';
 import CertificatsNumeriques from './pages/certificats-numeriques';
 import SuiviDesInstruments from './pages/suivi-des-instruments';
-import Login from './pages/login'; // <-- new import
+import Login from './pages/login';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <ErrorBoundary>
-      <ScrollToTop />
-      <RouterRoutes>
-        {/* Define your route here */}
-        <Route path="/" element={<TableauDeBord />} />
-        <Route path="/tableau-de-bord" element={<TableauDeBord />} />
-        <Route path="/maintenance-predictive" element={<MaintenancePredictive />} />
-        <Route path="/analyse-des-performances" element={<AnalyseDesPerformances />} />
-        <Route path="/cycles-de-sterilisation" element={<CyclesSterilisation />} />
-        <Route path="/certificats-numeriques" element={<CertificatsNumeriques />} />
-        <Route path="/suivi-des-instruments" element={<SuiviDesInstruments />} />
+        <ScrollToTop />
+        <RouterRoutes>
+          {/* Make Login the initial entry point */}
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* New route for Login */}
-        <Route path="/login" element={<Login />} />
+          {/* Application routes */}
+          <Route path="/tableau-de-bord" element={<TableauDeBord />} />
+          <Route path="/maintenance-predictive" element={<MaintenancePredictive />} />
+          <Route path="/analyse-des-performances" element={<AnalyseDesPerformances />} />
+          <Route path="/cycles-de-sterilisation" element={<CyclesSterilisation />} />
+          <Route path="/certificats-numeriques" element={<CertificatsNumeriques />} />
+          <Route path="/suivi-des-instruments" element={<SuiviDesInstruments />} />
 
-        {/* Fallback */}
-        <Route path="*" element={<NotFound />} />
-      </RouterRoutes>
+          {/* Fallback */}
+          <Route path="*" element={<NotFound />} />
+        </RouterRoutes>
       </ErrorBoundary>
     </BrowserRouter>
   );
